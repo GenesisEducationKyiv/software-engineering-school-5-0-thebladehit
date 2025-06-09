0163bdd28a705f6ebd5b4481016d0879dd0c49c1
thebladehit <boqdanyarmolka@gmail.com>
2025-06-09 15:55:10 +0300

    Add health type dto

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
+import { HealthDto } from './dto/health.dto';
+
 @Injectable()
 export class AppService {
-  getHealth(): { status: string; timestamp: string } {
+  getHealth(): HealthDto {
     return {
       status: 'OK',
       timestamp: new Date().toISOString(),
