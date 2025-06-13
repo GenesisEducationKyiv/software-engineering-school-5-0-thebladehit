de750fa883ddef48d451a3da8bc91cc8b52667e1
thebladehit <boqdanyarmolka@gmail.com>
2025-06-14 00:12:12 +0300

    Add Prisma module

--- src/modules/subscriptions/subscriptions.module.ts
+++ src/modules/subscriptions/subscriptions.module.ts
@@ -1,19 +1,18 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaService } from '../prisma/prisma.service';
 import { MailModule } from '../mail/mail.module';
 
 import { SubscriptionRepository } from './contracts/subscription.repository';
 import { SubscriptionRepositoryImpl } from './subscription.repositoryImpl';
 import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [MailModule],
+  imports: [MailModule, PrismaModule],
   controllers: [SubscriptionsController],
   providers: [
     SubscriptionsService,
-    PrismaService,
     {
       provide: SubscriptionRepository,
       useClass: SubscriptionRepositoryImpl,
