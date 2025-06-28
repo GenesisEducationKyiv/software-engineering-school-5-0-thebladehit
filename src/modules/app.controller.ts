6c52b6d1c609a9bd51b60165f924ab37051dd933
thebladehit <boqdanyarmolka@gmail.com>
2025-06-28 16:04:23 +0300

    Remove redundant dto

--- src/modules/app.controller.ts
+++ src/modules/app.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
-import { HealthDto } from './dto/health.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/health')
-  getHealth(): HealthDto {
+  getHealth(): { status: string } {
     return this.appService.getHealth();
   }
 }
