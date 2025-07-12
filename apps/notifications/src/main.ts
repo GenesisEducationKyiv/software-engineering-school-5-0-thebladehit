ccc31a79f628e45791d43cd18f3a091c427855db
thebladehit <boqdanyarmolka@gmail.com>
2025-07-12 13:37:53 +0300

    Add notification logic

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { NotificationsModule } from './notifications.module';
+import { WinstonModule } from 'nest-winston';
 
-async function bootstrap() {
-  const app = await NestFactory.create(NotificationsModule);
-  await app.listen(process.env.port ?? 3000);
+import { winstonConfig } from '@app/common/logger';
+
+import { NotificationModule } from './notification.module';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(NotificationModule, {
+    logger: WinstonModule.createLogger(winstonConfig),
+  });
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    })
+  );
+  await app.listen(configService.get('PORT') ?? 3002);
 }
+
 bootstrap();
---
+++ apps/notifications/src/notification.module.ts
@@ -0,0 +1,61 @@
+import { join } from 'path';
+
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import * as Joi from 'joi';
+
+import { AbstractNotificationService } from './abstracts/notification.service.abstract';
+import { MailService } from './mail.service';
+import { NotificationController } from './notification.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        SMTP_HOST: Joi.string().required(),
+        SMTP_PORT: Joi.number().required(),
+        SMTP_USERNAME: Joi.string().required(),
+        SMTP_PASSWORD: Joi.string().required(),
+        BACK_BASE_URL: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('SMTP_HOST'),
+          port: configService.get<number>('SMTP_PORT'),
+          secure: false,
+          auth: {
+            user: configService.get<string>('SMTP_USERNAME'),
+            pass: configService.get<string>('SMTP_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: '"No Reply" <noreply@example.com>',
+        },
+        template: {
+          dir: join(__dirname, '..', '..', 'mail', 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
+  controllers: [NotificationController],
+  providers: [
+    {
+      provide: AbstractNotificationService,
+      useClass: MailService,
+    },
+  ],
+  exports: [AbstractNotificationService],
+})
+export class NotificationModule {}
