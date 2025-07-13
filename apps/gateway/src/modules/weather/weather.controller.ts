1684ed9c62efdad02000c548a542cd2c9fe2d799
thebladehit <boqdanyarmolka@gmail.com>
2025-07-13 20:36:41 +0300

    Implement gateway microservice logic

---
+++ apps/gateway/src/modules/weather/weather.controller.ts
@@ -0,0 +1,19 @@
+import {
+  Controller,
+  Get,
+  Query,
+} from '@nestjs/common';
+
+import { CityQueryDto, WeatherResponseDto } from '@app/common/types';
+
+import { WeatherService } from './weather.service';
+
+@Controller('weather')
+export class WeatherController {
+  constructor(private readonly service: WeatherService) {}
+
+  @Get()
+  getWeather(@Query() query: CityQueryDto): Promise<WeatherResponseDto> {
+    return this.service.getWeather(query.city);
+  }
+}
