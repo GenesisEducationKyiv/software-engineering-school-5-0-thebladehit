3173ad495b7a7d9e3cd5a905e1533ec8daf96fdf
thebladehit <boqdanyarmolka@gmail.com>
2025-07-13 23:26:48 +0300

    Add e2e tests

--- apps/subscriptions/src/modules/notifier/notifier.module.ts
+++ apps/subscriptions/src/modules/notifier/notifier.module.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 
 import { NotificationsModule } from '../notifications/notifications.module';
